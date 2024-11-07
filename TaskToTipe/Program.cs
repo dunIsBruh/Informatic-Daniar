398dca2148373b9e0ae10aa4aaec49a577eff372
Daniyar Garafiev <daniargarafiev6@gmail.com>
2024-11-07 21:53:33 +0300

    Fix2 TipeOperation task

--- TaskToTipe/Program.cs
+++ TaskToTipe/Program.cs
@@ -4,17 +4,17 @@ class Program
 {
     static void Main(string[] args)
 	{
-		string name = Console.Readline();
-		int age = int.Parse(Console.Readline());
-		int course = int.Parse(Console.Readline());
-		string groupNumber = Console.Readline();
-		bool hasHomePet = false;
-		
-		Console.WriteLine("Ankete:");
-		Console.WriteLine("You name: " + name);
-		Console.WriteLine("You are " + age + " years old");
-		Console.WriteLine("You in " + course + " course and in " + groupNumber + " group");
-		Console.WriteLine("You has home pet:" + hasHomePet);
+	string name = Console.Readline();
+	int age = int.Parse(Console.Readline());
+	int course = int.Parse(Console.Readline());
+	string groupNumber = Console.Readline();
+	bool hasHomePet = false;
+	
+	Console.WriteLine("Ankete:");
+	Console.WriteLine("You name: " + name);
+	Console.WriteLine("You are " + age + " years old");
+	Console.WriteLine("You in " + course + " course and in " + groupNumber + " group");
+	Console.WriteLine("You has home pet:" + hasHomePet);
     }
 }
 
