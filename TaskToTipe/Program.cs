7bbd6a63c56a720a2d647d5f05140327c623fd5e
Daniyar Garafiev <daniargarafiev6@gmail.com>
2024-11-07 19:32:26 +0300

    Complete tipe task

--- TaskToTipe/Program.cs
+++ TaskToTipe/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TaskToTipe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.Readline();
+        int age = int.Parse(Console.Readline());
+        int course = int.Parse(Console.Readline());
+        string groupNumber = Console.Readline();
+		bool hasHomePet = false;
+		
+		Console.WriteLine("Ankete:");
+		Console.WriteLine("You name: " + name);
+		Console.WriteLine("You are " + age + " years old");
+		Console.WriteLine("You in " + course + " course and in " + groupNumber + " group");
+		Console.WriteLine("You has home pet:" + hasHomePet);
+    }
+}
+
